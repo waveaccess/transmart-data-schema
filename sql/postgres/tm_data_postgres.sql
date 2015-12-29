e7788209ab700af09dc8a5fcf5a6025e8d4e195e
waveaccess <info@wave-access.com>
2015-12-29 18:01:34 +0400

    Added required permissions.

--- sql/postgres/tm_data_postgres.sql
+++ sql/postgres/tm_data_postgres.sql
@@ -1,17 +1,19 @@
-﻿--Deleting existing user and schema for avoiding errors
+--Deleting existing user and schema for avoiding errors
 DROP SCHEMA IF EXISTS tm_data CASCADE;
 DROP ROLE IF EXISTS tm_data;
 
 --Creating user and schema
 CREATE ROLE tm_data LOGIN PASSWORD 'tm_data';
 CREATE SCHEMA AUTHORIZATION tm_data;
+GRANT ALL ON SCHEMA tm_data TO tm_data;
 
 --1) List of all studies
 CREATE OR REPLACE VIEW tm_data.studies AS
 SELECT 
-	DISTINCT(sourcesystem_cd) as study_id 
+	DISTINCT(sourcesystem_cd) AS study_id 
 FROM 
 	i2b2demodata.concept_dimension;
+GRANT select ON tm_data.studies TO tm_data;
 
 --2) List of all patients
 CREATE OR REPLACE VIEW tm_data.studies_patients AS
@@ -30,6 +32,7 @@ SELECT
 	pd.update_date AS update_date
 FROM
 	i2b2demodata.patient_dimension pd;
+GRANT select ON tm_data.studies_patients TO tm_data;
 
 --3) List of clinical attributes
 CREATE OR REPLACE VIEW tm_data.studies_clinical_attributes AS
@@ -54,6 +57,7 @@ WHERE
 		SELECT DISTINCT sm.concept_code FROM deapp.de_subject_sample_mapping sm
 	)
 );
+GRANT select ON tm_data.studies_clinical_attributes TO tm_data;
 
 --4) List of clinical values
 CREATE OR REPLACE VIEW tm_data.studies_clinical_values AS
@@ -88,3 +92,4 @@ WHERE
 		SELECT DISTINCT sm.concept_code FROM deapp.de_subject_sample_mapping sm
 	)
 );
+GRANT select ON tm_data.studies_clinical_values TO tm_data;
